7b9ee1cd075ced7ef5b47a08a1f5e35ed40657ea
v.nair <vinayak@live-eo.com>
2024-04-17 15:55:22 +0200

    add cdk.json

---
+++ deployment/profile.py
@@ -0,0 +1,9 @@
+from pydantic import PositiveInt
+from pydantic_settings import BaseSettings
+
+
+class DeploymentProfile(BaseSettings):
+    memory: PositiveInt = 1024
+    timeout: PositiveInt = 30
+    aws_ecr_repository_arn: str
+    image_tag_or_digest: str = "latest"
