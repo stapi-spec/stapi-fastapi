1f18d3002090141e83940d110390babc9d8fbd91
v.nair <vinayak@live-eo.com>
2024-04-17 15:54:21 +0200

    remove unnecessary files

--- deployment/app.py
+++ deployment/app.py
@@ -1,26 +1,28 @@
-from aws_cdk import App, Duration, CfnOutput, Stack
-from aws_cdk import aws_logs as logs
-import aws_cdk.aws_lambda as _lambda
+import os
+from typing import Any, Dict, Optional
+
 import aws_cdk.aws_apigatewayv2_alpha as apigatewayv2
 import aws_cdk.aws_apigatewayv2_integrations_alpha as apigateway_integrations
-from typing import Any, Optional, Dict
-import os
+import aws_cdk.aws_lambda as _lambda
+from aws_cdk import App, CfnOutput, Duration, Stack
+from aws_cdk import aws_logs as logs
 from constructs import Construct
 
-class STATDeploy(Stack):
 
-    def __init__(self, scope: Construct, 
+class STATDeploy(Stack):
+    def __init__(
+        self,
+        scope: Construct,
         id: str,
         memory: int = 1024,
         timeout: int = 30,
-        runtime: _lambda.Runtime = _lambda.Runtime.PYTHON_3_11, #Set to 3.11 as the aws_cdk version does not have 3.12
+        runtime: _lambda.Runtime = _lambda.Runtime.PYTHON_3_11,  # Set to 3.11 as the aws_cdk version does not have 3.12
         concurrent: Optional[int] = None,
         environment: Optional[Dict] = None,
-        code_dir:str = "/",
-        **kwargs:Any) -> None:
+        code_dir: str = "/",
+        **kwargs: Any,
+    ) -> None:
         super().__init__(scope, id, **kwargs)
-
-
 
         lambda_function = _lambda.Function(
             self,
@@ -35,7 +37,7 @@ class STATDeploy(Stack):
             reserved_concurrent_executions=concurrent,
             timeout=Duration.seconds(timeout),
             environment=environment,
-            log_retention=logs.RetentionDays.ONE_WEEK, #Honestly can be removed not sure if required at this stage
+            log_retention=logs.RetentionDays.ONE_WEEK,  # Honestly can be removed not sure if required at this stage
         )
 
         api = apigatewayv2.HttpApi(
@@ -47,12 +49,12 @@ class STATDeploy(Stack):
         )
         CfnOutput(self, "Endpoint", value=api.url)
 
+
 def main():
     app = App()
     STATDeploy(app, "MyCdkProjectStack")
     app.synth()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
