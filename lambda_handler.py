5f4e7f866a5dc05b380470f3b55ea23c723c25e6
Julia Signell <jsignell@gmail.com>
2024-04-18 08:31:36 -0400

    Make it possible to use different backends

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,10 +1,33 @@
+import os
+
 from fastapi import FastAPI
 from mangum import Mangum
 
 from stat_fastapi.api import StatApiRouter
-from stat_fastapi_landsat import StatLandsatBackend
+
+
+def get_backend(name: str):
+    if name == "landsat":
+        from stat_fastapi_landsat import StatLandsatBackend
+
+        return StatLandsatBackend()
+    if name == "blacksky":
+        from stat_fastapi_blacksky import BlackskyBackend
+
+        return BlackskyBackend()
+    if name == "up42":
+        from stat_fastapi_up42 import StatUp42Backend
+
+        return StatUp42Backend()
+    if name == "umbra":
+        from stat_fastapi_umbra import UmbraBackend
+
+        return UmbraBackend
+
 
 app = FastAPI()
-app.include_router(StatApiRouter(backend=StatLandsatBackend()).router)
+app.include_router(
+    StatApiRouter(backend=get_backend(os.environ.get("BACKEND_NAME"))).router
+)
 
 handler = Mangum(app, lifespan="off")
