07b78041c074db191813516f9d23072fae6128ee
Christian Wygoda <christian.wygoda@satellitevu.com>
2024-05-09 13:25:02 +0200

    refactor: make backend selection more flexible

--- stapi_fastapi/__dev__.py
+++ stapi_fastapi/__dev__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from importlib import import_module
 from sys import stderr
 
 from fastapi import FastAPI
+from pydantic import field_validator
 
 try:
     from pydantic_settings import BaseSettings
@@ -12,20 +14,31 @@ except ImportError:
     exit(1)
 
 from stapi_fastapi.api import StapiRouter
-from stapi_fastapi_tle_backend import StapiMockBackend
 
 
 class DevSettings(BaseSettings):
     port: int = 8000
     host: str = "127.0.0.1"
+    backend_name: str = "stapi_fastapi_landsat:LandsatBackend"
+
+    @field_validator("backend_name", mode="after")
+    def validate_backend(cls, value: str):
+        try:
+            mod, attr = value.split(":", maxsplit=1)
+            mod = import_module(mod)
+            attr = getattr(mod, attr)
+        except ImportError as exc:
+            raise ValueError("not a valid backend") from exc
+        return attr
 
 
+settings = DevSettings()
+backend = settings.backend_name()
 app = FastAPI(debug=True)
-app.include_router(StapiRouter(backend=StapiMockBackend()).router)
+app.include_router(StapiRouter(backend).router)
 
 
 def cli():
-    settings = DevSettings()
     run(
         "stapi_fastapi.__dev__:app", reload=True, host=settings.host, port=settings.port
     )
