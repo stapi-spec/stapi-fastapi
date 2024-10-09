080ad6db9b971a38a6c1dcba9d61347a86e50abe
Andy Fink-Miller <andrew.fink-miller@umbra.space>
2024-10-09 15:27:32 -0600

    Add links to Opportunity to match spec

--- src/stapi_fastapi/models/opportunity.py
+++ src/stapi_fastapi/models/opportunity.py
@@ -4,6 +4,7 @@ from geojson_pydantic import Feature, FeatureCollection
 from geojson_pydantic.geometries import Geometry
 from pydantic import BaseModel, ConfigDict
 
+from stapi_fastapi.models.shared import Link
 from stapi_fastapi.types.datetime_interval import DatetimeInterval
 from stapi_fastapi.types.filter import CQL2Filter
 
@@ -22,6 +23,7 @@ class OpportunityRequest(OpportunityProperties):
 
 class Opportunity(Feature[Geometry, OpportunityProperties]):
     type: Literal["Feature"] = "Feature"
+    links: list[Link] = []
 
 
 class OpportunityCollection(FeatureCollection[Opportunity]):
