2939a3e9cc06a6c8b01ed317b3ac31575bc0d0c8
Jarrett Keifer <jkeifer0@gmail.com>
2024-11-01 14:35:23 -0700

    fix GeoJSONResponse model bug per openapi docs

--- src/stapi_fastapi/responses.py
+++ src/stapi_fastapi/responses.py
@@ -4,6 +4,4 @@ from stapi_fastapi.constants import TYPE_GEOJSON
 
 
 class GeoJSONResponse(JSONResponse):
-    def __init__(self, *args, **kwargs) -> None:
-        kwargs["media_type"] = TYPE_GEOJSON
-        super().__init__(*args, **kwargs)
+    media_type = TYPE_GEOJSON
