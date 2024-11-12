507fb90d12b086ec2335161e2739bb0466f94176
Phil Varner <pvarner.ctr@gmail.com>
2024-11-12 09:53:30 -0500

    remove init method from Conformance

--- src/stapi_fastapi/models/conformance.py
+++ src/stapi_fastapi/models/conformance.py
@@ -5,13 +5,4 @@ OPPORTUNITIES = "https://stapi.example.com/v0.1.0/opportunities"
 
 
 class Conformance(BaseModel):
-    conforms_to: list[str] = Field(default_factory=list, alias="conformsTo")
-
-    def __init__(
-        self,
-        *args,
-        conforms_to: list[str],
-        **kwargs,
-    ) -> None:
-        super().__init__(*args, **kwargs)
-        self.conforms_to = conforms_to
+    conforms_to: list[str] = Field(default_factory=list, serialization_alias="conformsTo")
