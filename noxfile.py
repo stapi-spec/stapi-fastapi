92bfb3f2c870ec7974eb2985aeefaf97a95b6e4c
pjhartzell <preston.hartzell@gmail.com>
2025-01-27 20:10:29 -0600

    fix: allow python 3.12.0 and above

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.12"])
+@nox.session(python=["3.12", "3.13"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("poetry", "run", "pytest", external=True)
